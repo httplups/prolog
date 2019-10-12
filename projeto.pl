5395adcaf911834dc3573713eb734aa0543f5c96
Luana Barros <dev.luana@gmail.com>
2019-10-12 00:17:40 -0300

    comentei as funções

--- projeto.pl
+++ projeto.pl
@@ -20,28 +20,21 @@ topo:-
 
   getInter([_, B | R]) :- write_ln([B | R]).
 
-% testloop2(0).
-% testloop2(N) :- N>0, write("YYY : "), write(N), nl, M is N-1, testloop2(M).
-
-% testloop(0).
-% testloop(N) :- N>0, write("XXX : "), write_ln(N), M is N-1, testloop2(M), testloop(M).
-% inter_circ_circ(A,B)
-  for2(_,[]).
-  for2(A,[B|R]) :- write(A), write(B), nl, inter(A,B), for2(A, R).
-
+  % percorre a lista fazendo a combinação de cada figura em pares
   for1([]).
   for1([A|R]) :- for2(A, R), for1(R).
+  for2(_,[]).
+  for2(A,[B|R]) :- write(A), write(B), nl, inter(A,B), for2(A, R). %chama função inter que testa a intersecção das figuras
+
+  
 
   inter(A,B) :- inter_circ_circ(A,B).
   inter(A,B) :- inter_quad_quad(A,B).
   inter(A,B) :- inter_circ_quad(A,B).
   
-
+  % por enquanto retorna true apenas para dar certo e o for prosseguir
   inter_circ_quad(_,_) :- true.
-  inter_quad_quad(A, B) :- set_coord(A, CooA), set_coord(B, CooB), is_inter_quad(CooB, CooA).
 
-  % inter_quad_quad(A,B) :- true.
-% inter_circ_circ(circ(_,X1,_,R1), circ(_,X2,_,R2)) :- true, !.
   inter_circ_circ(circ(_,X1,_,R1), circ(_,X2,_,R2)) :- ((X2 - R2) - (X1 + R1)) < 0
                                                           -> write_ln("Yes")
                                                           ; write_ln("No").
@@ -50,43 +43,42 @@ topo:-
                                                           -> write_ln("Yes")
                                                           ; write_ln("No").
 
-set_coord(quad(N, X, Y, L), coord(N,A,B,C,D)) :- A = [AX,AY], AX is X - (L/2), AY is Y - (L/2),
-                                                 B = [BX,AY], BX is X + (L/2),
-                                                 C = [BX,CY], CY is Y + (L/2),
-                                                 D = [AX,CY].
+  inter_quad_quad(A, B) :- set_coord(A, CooA), set_coord(B, CooB), is_inter_quad(CooB, CooA).
+
+  set_coord(quad(N, X, Y, L), coord(N,A,B,C,D)) :- A = [AX,AY], AX is X - (L/2), AY is Y - (L/2),
+                                                  B = [BX,AY], BX is X + (L/2),
+                                                  C = [BX,CY], CY is Y + (L/2),
+                                                  D = [AX,CY].
+
+  is_inter_quad(coord(Q1,A1,B1,_,D1), coord(Q2,A2,B2,_,D2)) :-  get_elem_pos(A2,0,A2X), % verifica na horizontal
+                                                                get_elem_pos(B2,0,B2X),
+                                                                get_elem_pos(A1,0,A1X),
+                                                                get_elem_pos(B1,0,B1X),
+                                                                verifica_x(A2X,B2X,A1X,B1X, Cond1),
+                                                              %  write_ln(A2X), write_ln(B2X), write_ln(A1X), write_ln(B1X),
+
+                                                                get_elem_pos(D2,1,D2Y),
+                                                                get_elem_pos(A2,1,A2Y),
+                                                                get_elem_pos(A1,1,A1Y),
+                                                                get_elem_pos(D1,1,D1Y),
+                                                                verifica_y(D2Y,A2Y,D1Y,A1Y, Cond2),
+                                                              %  write_ln(D2Y), write_ln(A2Y), write_ln(D1Y), write_ln(A1Y),
+                                                              %  write_ln(Cond1),
+                                                              %  write_ln(Cond2),
+                                                                write("Foi"), write(" "), write(Q2),write(" "), write_ln(Q1).
 
 
-is_inter_quad(coord(Q1,A1,B1,_,D1), coord(Q2,A2,B2,_,D2)) :-  get_elem_pos(A2,0,A2X), % verifica na horizontal
-                                                              get_elem_pos(B2,0,B2X),
-                                                              get_elem_pos(A1,0,A1X),
-                                                              get_elem_pos(B1,0,B1X),
-                                                              verifica_x(A2X,B2X,A1X,B1X, Cond1),
-                                                            %  write_ln(A2X), write_ln(B2X), write_ln(A1X), write_ln(B1X),
+  % retorna os elementos do par ordenado do plano cartesiano
+  get_elem_pos([],_, _) :- fail, !.
+  get_elem_pos(_, N, _) :- N > 1, !,fail.
+  get_elem_pos([X, _], 0, X).
+  get_elem_pos([_, Y], 1, Y).
 
-                                                              get_elem_pos(D2,1,D2Y),
-                                                              get_elem_pos(A2,1,A2Y),
-                                                              get_elem_pos(A1,1,A1Y),
-                                                              get_elem_pos(D1,1,D1Y),
-                                                              verifica_y(D2Y,A2Y,D1Y,A1Y, Cond2),
-                                                            %  write_ln(D2Y), write_ln(A2Y), write_ln(D1Y), write_ln(A1Y),
-                                                            %  write_ln(Cond1),
-                                                            %  write_ln(Cond2),
-                                                              write("Foi"), write(" "), write(Q2),write(" "), write_ln(Q1).
+  %  verifica se os pontos A2X ou B2X estão entre os pontos A1X e B1X.
+  verifica_x(A2X,_,A1X,B1X, true) :- (A2X >= A1X, A2X =< B1X).
+  verifica_x(_,B2X,A1X,B1X, true) :- (B2X >= A1X, B2X =< B1X).
+  verifica_x(_,_,_,_, fail).
 
-
-
-
-% retorna os elementos do par ordenado do plano cartesiano
-get_elem_pos([],_, _) :- fail, !.
-get_elem_pos(_, N, _) :- N > 1, !,fail.
-get_elem_pos([X, _], 0, X).
-get_elem_pos([_, Y], 1, Y).
-
-%  verifica se os pontos A2X ou B2X estão entre os pontos A1X e B1X.
-verifica_x(A2X,_,A1X,B1X, true) :- (A2X >= A1X, A2X =< B1X).
-verifica_x(_,B2X,A1X,B1X, true) :- (B2X >= A1X, B2X =< B1X).
-verifica_x(_,_,_,_, fail).
-
-verifica_y(D2Y,_,D1Y,A1Y, true) :- (D2Y =< D1Y, D2Y >= A1Y). 
-verifica_y(_,A2Y,D1Y,A1Y, true) :- (A2Y =< D1Y, A2Y >= A1Y). 
-verifica_y(_,_,_,_, fail).
+  verifica_y(D2Y,_,D1Y,A1Y, true) :- (D2Y =< D1Y, D2Y >= A1Y). 
+  verifica_y(_,A2Y,D1Y,A1Y, true) :- (A2Y =< D1Y, A2Y >= A1Y). 
+  verifica_y(_,_,_,_, fail).
